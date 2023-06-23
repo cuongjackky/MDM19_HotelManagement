c4049b4c10a34b06940d48acd6c020f24a94e42d
Cuong <72879427+cuongjackky@users.noreply.github.com>
2023-06-23 23:49:42 +0700

    Update db.php

--- Source/configs/db.php
+++ Source/configs/db.php
@@ -7,20 +7,22 @@ use MongoDB\Driver\Cursor;
 use MongoDB\Driver\Manager;
 
 class DB{
-    
+    private static $mongoInstance = NULL;
     private static $redisInstance = NULL;
     // config Mongodb
     public static function getMongoDBInstance($database,$collection){
-        $mongoInstance = NULL;
-        try {
-            $mongoClient = new MongoDB\Client('mongodb://localhost:27017'); // Thay đổi host và port nếu cần thiết
-            $database = $mongoClient->$database;
-            $mongoInstance = $database->$collection;
-        
-        } catch (Exception $ex) {
-            die($ex->getMessage());
+        if (!isset(self::$mongoInstance))
+            try {
+                $mongoClient = new MongoDB\Client('mongodb://localhost:27017'); // Thay đổi host và port nếu cần thiết
+                
+            
+            } catch (Exception $ex) {
+                die($ex->getMessage());
+            }
         }
-        return $mongoInstance;
+        $database = $mongoClient->$database;
+        self::$mongoInstance = $database->$collection;
+        return self::$mongoInstance;
    
     }
 
@@ -34,8 +36,8 @@ class DB{
 
         if (!isset(self::$redisInstance)) {
             try {
-                $redisInstance = new Redis();
-                $redisInstance->connect('localhost', 6379);
+                self::$redisInstance = new Redis();
+                self::$redisInstance->connect('localhost', 6379);
            
             } catch (Exception $ex) {
                 die($ex->getMessage());
